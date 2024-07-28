f970fd19a19e64682643932303bc774aa7037945
Decembboy <debor9130@gmail.com>
2024-07-28 12:19:40 +0530

    Add files via upload

---
+++ allow the user to write unless word count announced.py
@@ -0,0 +1,39 @@
+li=[]
+enter=""
+while enter !="count":
+    enter=input(" ") #Type count to stop the loop#
+    li.append(enter)
+print("User have enterd",len(li),"times")
+    
+    
+
+
+
+
+
+
+
+
+    
+
+
+
+     
+            
+  
+
+
+    
+   
+    
+
+    
+    
+
+
+
+
+
+
+        
+
